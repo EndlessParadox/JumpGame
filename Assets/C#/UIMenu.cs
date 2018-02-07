72fa106b0c1005e70c359d6753089b8955a56553
EndlessParadox <kanzaki_h_aria@foxmail.com>
2018-02-07 15:35:26 +0800

    按键监听

---
+++ Assets/C#/UIMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIMenu : MonoBehaviour {
+
+    public Button btnExit;
+
+    public Button btnRevive;
+	// Use this for initialization
+	void Start () {
+        btnExit.onClick.AddListener(Exit);
+	}
+
+    void Exit()
+    {
+        Debug.LogWarning("Close!!!");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

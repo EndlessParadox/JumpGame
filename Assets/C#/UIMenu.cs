1bc40789abd4eca9fb6b7f3a257754ea7fec4b51
shui <1137893431@qq.com>
2018-02-08 14:32:27 +0800

    test

--- Assets/C#/UIMenu.cs
+++ Assets/C#/UIMenu.cs
@@ -11,21 +11,32 @@ public class UIMenu : MonoBehaviour {
     public Button btnRevive;
 	// Use this for initialization
 	void Start () {
-        btnExit.onClick.AddListener(Exit);
-  
+
+       btnExit.onClick.AddListener(Exit);
+       btnRevive.onClick.AddListener(Revive);
+
+    }
+
+    void Revive()
+    {
+        Button btn = btnRevive.GetComponent<Button>();
+        SceneManager.LoadScene("jump");
 
     }
 
     void Exit()
     {
-        Debug.LogWarning("Close!!!");
+        //Debug.LogWarning("Close!!!");
+
+        Button btn = btnExit.GetComponent<Button>();
+        Application.Quit();
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetMouseButton(0))
-            SceneManager.LoadScene("jump");
-        if (Input.GetMouseButton(1))
-            Application.Quit();
+        //if (Input.GetMouseButton(0))
+        //    SceneManager.LoadScene("jump");
+        //if (Input.GetMouseButton(1))
+        //    Application.Quit();
     }
 }
